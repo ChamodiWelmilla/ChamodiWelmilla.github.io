66315f9eded6da6bc31c1b94c0d82203def3aa19
Chamodi Welmilla <chamodiwelmilla99@gmail.com>
2025-12-16 16:03:08 +0530

    update experiences

--- src/data/Experience.jsx
+++ src/data/Experience.jsx
@@ -2,12 +2,12 @@ export const experiences = [
   {
     title: "Full-stack Developer Intern",
     company: "Creative Software",
-    duration: "Dec 2024 - May 2024",
+    duration: "Dec 2024 - Oct 2025",
   },
   {
     title: "Web Master",
     company: "IEEE WIE Student Branch Affinity Group of University of Moratuwa",
-    duration: "Oct 2024 - Present",
+    duration: "2024/2025",
   },
   {
     title: "Full-stack Developer",
